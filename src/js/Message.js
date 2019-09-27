3fdf9ee7572966c6aae0190265de0cddd915e91a
James Collerton <james.collerton@bbc.co.uk>
2019-09-27 16:31:06 +0100

    Got Jest test working

---
+++ src/js/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className="content">
+        <h1>Express and Webpack App</h1>
+        <p className="description">Example application for using Express and Webpack.</p>
+    </div>
+  )
+}
+export default Message
