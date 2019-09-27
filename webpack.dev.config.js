0d180547c6ed1d18480b590b2ad73f807c5304e2
James Collerton <james.collerton@bbc.co.uk>
2019-09-27 13:38:36 +0100

    Added eslint

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/index.js']
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -15,6 +15,17 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true,
+          failOnError: false,
+          failOnWarning: false
+        }
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -47,6 +58,7 @@ module.exports = {
       filename: "./index.html",
       excludeChunks: [ 'server' ]
     }),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   ]
 }
