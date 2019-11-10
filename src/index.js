b0567268be23c36246eed2240ae28d5b4c45050f
James Collerton <jamescollerton@live.co.uk>
2019-11-10 09:09:16 +0000

    Added basic bootstrap header

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom'
 import Message from './js/Message'
 import './css/style.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
 /*
     This attaches to the HTML element 'react-container' and is used to inject the react into the page.
 */
--- src/js/Message.js
+++ src/js/Message.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const Message = () => {
   return (
     <div className="content">
+        <NavBar />
         <h1>Express and Webpack App</h1>
         <p className="description">Example application for using Express and Webpack.</p>
     </div>
