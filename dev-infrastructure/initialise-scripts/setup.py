b9d75c37399569ae71a976500a0efbd612a33cbe
James Collerton <james.collerton@bbc.co.uk>
2019-12-03 14:46:17 +0000

    Need to create docker image for running initialisation script

---
+++ dev-infrastructure/initialise-scripts/setup.py
@@ -0,0 +1,3 @@
+import boto3
+
+dynamo = boto3.client('dynamodb', region_name = 'eu-west-1', aws_access_key_id = 'access_key', aws_secret_access_key = 'secret_key', endpoint_url = 'http://localhost:4569')
