aac4af3c95d5c54c7d4d57da85576b870369f6f3
James Collerton <james.collerton@bbc.co.uk>
2019-12-03 15:56:44 +0000

    Failing images, but on the right track

--- dev-infrastructure/populater-image/initialise-scripts/setup.py
+++ dev-infrastructure/populater-image/initialise-scripts/setup.py
@@ -1,3 +1,11 @@
 import boto3
 
-dynamo = boto3.client('dynamodb', region_name = 'eu-west-1', aws_access_key_id = 'access_key', aws_secret_access_key = 'secret_key', endpoint_url = 'http://localhost:4569')
+dynamo = boto3.client(
+            'dynamodb', 
+            region_name = 'eu-west-1', 
+            aws_access_key_id = 'access_key', 
+            aws_secret_access_key = 'secret_key', 
+            endpoint_url = 'http://localhost:4569'
+        )
+
+dynamo.list_tables()
