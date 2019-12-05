f10b5cdfd78daef9ba203d36b9aebdb33d384938
James Collerton <james.collerton@bbc.co.uk>
2019-12-05 10:52:41 +0000

    Got the populating docker working

--- dev-infrastructure/populater-image/initialise-scripts/setup.py
+++ dev-infrastructure/populater-image/initialise-scripts/setup.py
@@ -5,7 +5,28 @@ dynamo = boto3.client(
             region_name = 'eu-west-1', 
             aws_access_key_id = 'access_key', 
             aws_secret_access_key = 'secret_key', 
-            endpoint_url = 'http://localhost:4569'
+            endpoint_url = 'http://localstack:4569'
         )
 
-dynamo.list_tables()
+dynamo.create_table(
+    TableName="Test",
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'TestAttribute',
+            'AttributeType': 'S'
+        },
+    ],
+    KeySchema=[
+        {
+            'AttributeName': 'TestAttribute',
+            'KeyType': 'HASH'
+        },
+    ],
+    # TODO: Can probably revise these
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 123,
+        'WriteCapacityUnits': 123
+    }
+)
+
+print(dynamo.list_tables())
